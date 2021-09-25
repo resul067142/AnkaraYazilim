eba8e638ad69d0b4b8e12977351b0eed2d4a5aa0
RESUL AKTAŞ <82091977+resul067142@users.noreply.github.com>
2021-09-25 21:47:54 +0300

    Add project files.

---
+++ Data/Migrations/20210809192333_ınitialmig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjeYonetim.Data.Migrations
+{
+    public partial class ınitialmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
